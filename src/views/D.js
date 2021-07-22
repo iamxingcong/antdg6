ed7ba9015c95f191a26d2535634e37a5aa941c80
v_coxing <v_coxing@tencent.com>
2021-07-22 15:34:22 +0800

    update

--- src/views/D.js
+++ src/views/D.js
@@ -11,44 +11,64 @@ const D = () => {
 
   const [treeData, setItems] = useState({});
 
- 
+
 
   useEffect(() => {
 
-  async function getRates() {
-      
-   const response = await fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/relations.json')
-  
-   const postdata = await response.json()
-   setItems(postdata)
+    async function getRates() {
 
-  
-    
-     
-      
- 
+      const response = await fetch('https://gw.alipayobjects.com/os/antvdemo/assets/data/relations.json')
+
+      const postdata = await response.json()
+      setItems(postdata)
 
 
-       graph = new G6.Graph({
+
+      console.log(treeData)
+
+
+
+
+      graph = new G6.Graph({
         container: ReactDOM.findDOMNode(ref.current),
-        width: 1600,
-        height: 600,
+        width: 1500,
+        height: 900,
+
+
         layout: {
-          type: 'random',
+          type: 'concentric',
+          center: [500, 400],     // 可选，
+          linkDistance: 150,         // 可选，边长
+          preventOverlap: true,     // 可选，必须配合 nodeSize
+          nodeSize: 30,             // 可选
+          sweep: 10,                // 可选
+          equidistant: false,       // 可选
+          startAngle: 0,            // 可选
+          clockwise: false,         // 可选
+          maxLevelDiff: 40,         // 可选
+          sortBy: 'degree',         // 可选
+          workerEnabled: true
+
         },
-        
+
         animate: true,
-        modes: 
-            
-            'zoom-canvas',
-        
-      
+        modes: 'zoom-canvas',
+        defaultNode: {
+          size: 26,
+          anchorPoints: [
+            [0, 0.5],
+            [1, 0.5],
+          ],
+        },
+
         defaultEdge: {
           type: 'cubic-horizontal',
         },
 
 
       });
+
+
       graph.node(function (node) {
         return {
           label: node.id,
@@ -58,14 +78,17 @@ const D = () => {
           },
         };
       });
+
+
       graph.data(postdata);
-    graph.render();
-   
-  }
- 
+      graph.render();
+
+
+    }
+
     getRates();
 
- 
+
   }, [])
 
 
