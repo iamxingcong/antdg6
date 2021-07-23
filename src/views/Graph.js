7ea169be08dd5e4daf91c301bdff6e3679f35303
v_coxing <v_coxing@tencent.com>
2021-07-23 15:42:54 +0800

    update

--- src/views/Graph.js
+++ src/views/Graph.js
@@ -7,8 +7,8 @@ class Graph extends React.Component {
     super(props);
     this.state = {
 
-      items:[]
-       
+      items: []
+
 
     };
   }
@@ -31,22 +31,70 @@ class Graph extends React.Component {
 
       console.log(this.state.items)
 
-      
+
       let graph = new G6.Graph({
         container: document.getElementById('axt'),
         width: window.screen.width,
         height: 1300,
+
+        linkCenter: true,
         modes: {
-          default: ["drag-canvas", "zoom-canvas", "drag-node"]
-        }
+          default: [
+            {
+              type: 'collapse-expand',
+              onChange: function onChange(item, collapsed) {
+                const data = item.getModel();
+                data.collapsed = collapsed;
+                return true;
+              },
+            },
+            'drag-canvas',
+            'zoom-canvas',
+          ],
+        },
+        
+       
+        layout: {
+          type: 'dagre',
+          rankdir: 'LR',
+          nodesep: 30,
+          ranksep: 100,
+      
+  
+          getId: function getId(d) {
+            return d.id;
+          },
+          getHeight: function getHeight() {
+            return 16;
+          },
+          getWidth: function getWidth() {
+            return 66;
+          },
+          getVGap: function getVGap() {
+            return 80;
+          },
+          getHGap: function getHGap() {
+            return 20;
+          },
+        },
       });
 
       graph.node(function (node) {
+        let position = 'right';
+        let rotate = 0;
+        if (!node.children) {
+          position = 'bottom';
+          //rotate = Math.PI / 2;
+        }
         return {
-          label: node.avgName,
+          label: node.name,
           labelCfg: {
-            offset: 10,
-            position: node.children && node.children.length > 0 ? 'left' : 'right',
+            position,
+            offset: 5,
+            style: {
+              rotate,
+              textAlign: 'start',
+            },
           },
         };
       });
